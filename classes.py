11f8572378db32911a6de94c90213f613f29b7ad
Alexey <suxovarov.alexey@gmail.com>
2022-06-11 16:11:16 +0300

    Change all

--- classes.py
+++ classes.py
@@ -1,54 +1,55 @@
 class IPAdress:
     @classmethod
-    def class_type(cls, bin_mask):
+    def class_type(cls, binmsk):
+        bin_mask = binmsk.split('.')
         if bin_mask[0][0] == '0':
-            return 24
+            return 24, '255.0.0.0'
 
         elif bin_mask[0][:2] == '10':
-            return 16
+            return 16, '255.255.0.0'
 
         elif bin_mask[0][:3] == '110':
-            return 8
+            return 8, '255.255.255.0'
 
         elif bin_mask[0][:4] == '1110':
-            return -1
+            return -1, '-1'
 
         elif bin_mask[0][:5] == '11110':
-            return -2
+            return -2, '-2'
+
+    def dec_to_bin(self):
+        return '.'.join([str(bin(int(octet)))[2:].zfill(8) for octet in self.str_adr.split('.')])
 
     def __init__(self, adr):
         self.str_adr = adr
-        self.split_mask = adr.split('.')
-        print(self.split_mask)
-        self.bin_split_mask = [str(bin(int(octet)))[2:].zfill(8) for octet in self.split_mask]
-        print(self.bin_split_mask)
-        self.subnet_class = self.class_type(self.bin_split_mask)
+        self.bin_split_mask = self.dec_to_bin()
+        self.net_class, self.net_mask = self.class_type(self.bin_split_mask)
 
     def __str__(self):
-        if self.subnet_class == 24:
+        if self.net_class == 24:
             return """Network class - A
                 Subnet mask - 255.0.0.0
                 Starting address - 1.0.0.0\tEnd address - 126.255.255.255
                 Number of possible subnets - 126\tNumber of possible hosts - 16 777 214\n"""
 
-        elif self.subnet_class == 16:
+        elif self.net_class == 16:
             return """Network class - B
                 Subnet mask - 255.255.0.0
                 Starting address - 128.0.0.0\tEnd address - 191.255.255.255
                 Number of possible subnets - 16 384\tNumber of possible hosts - 65 534\n"""
 
-        elif self.subnet_class == 8:
+        elif self.net_class == 8:
             return """Network class - C
                     Subnet mask - 255.255.255.0
                     Starting address - 192.0.0.0\tEnd address - 223.255.255.255
                     Number of possible subnets - 2 097 152\tNumber of possible hosts - 254\n"""
 
-        elif self.subnet_class == -1:
+        elif self.net_class == -1:
             return """Network class - D
                     Group addresses
                     Starting address - 224.0.0.0\tEnd address - 239.255.255.255\n"""
 
-        elif self.subnet_class == -2:
+        elif self.net_class == -2:
             return """Network class - E
                     Reserved addresses
                     Starting address - 240.0.0.0\tEnd address - 225.255.255.255\n"""
@@ -58,8 +59,8 @@ class IPAdress:
         subnet_bits, bits_for_host = len(bin(number_of_networks)[2:]), len(bin(number_of_hosts)[2:])
         print(subnet_bits, bits_for_host)
 
-        if subnet_bits + bits_for_host <= self.subnet_class:
-            print(f"255.255.{int('1' * subnet_bits, 2) << (8 - subnet_bits)}.0")
+        if subnet_bits + bits_for_host <= self.net_class:
+
             return subnet_bits, bits_for_host
 
         else:
